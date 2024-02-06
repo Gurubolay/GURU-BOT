d9f983afa40dd01fdc41ba908275352fac91ea66
Gurubolay <159178889+Gurubolay@users.noreply.github.com>
2024-02-07 02:00:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['916392197322', 'GURU', true],
+  ['916392197322', 'Ash', true], 
   [''] 
 ] //Number of owners
 
